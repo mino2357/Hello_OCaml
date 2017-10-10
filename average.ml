272946dfe60a0676efc480d4155103fbb6cf04e8
mino2357 <mino2357@gmail.com>
2017-10-11 00:46:35 +0900

    fib

--- average.ml
+++ average.ml
@@ -1,8 +1,17 @@
 let average x y = let sum =  x +. y in sum /. 2.;;
 
 let rec fact n =
-    if n = 0 then 1 else n * fact (n - 1);;
+  if n = 0 then 1 else n * fact (n - 1);;
+
+let rec pow (x, n) =
+  if n = 0 then 1 else x * pow (x, n - 1);;
+
+let rec fib n =
+  if n = 0 || n = 1 then 1
+  else fib (n - 1) + fib (n - 2)
 
 
 let () = Printf.printf "%f\n" (average 2. 3.);;
 let () = Printf.printf "%d\n" (fact 10);;
+let () = Printf.printf "%d\n" (pow (2, 3));;
+let () = Printf.printf "%d\n" (fib 20);;
